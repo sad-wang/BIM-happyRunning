b6fd46f13eeb4af29adf1cd0ae8952cc37a9d1da
Leown <leown_wang@163.com>
2019-08-29 15:17:20 +0800

    refactored moocLists pages

--- pages/mooc/moocLists/moocLists.js
+++ pages/mooc/moocLists/moocLists.js
@@ -1,9 +1,5 @@
 // pages/mooc/moocLists/moocLists.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     moocLists: [
       {
@@ -89,66 +85,9 @@ Page({
       }
     ],
   },
-
-
   navTo:function(e) {
     wx.navigateTo({
       url: e.currentTarget.dataset.url,
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
