4da8c773424fd9d898d0688845820ab63cbb5e15
Leown <leown_wang@163.com>
2019-08-29 14:55:37 +0800

    refactored moocDetails pages

--- pages/mooc/moocDetails/moocDetails.js
+++ pages/mooc/moocDetails/moocDetails.js
@@ -1,13 +1,8 @@
 // pages/mooc/moocDetails/moocDetails.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     currentTab: 0
   },
-
   clickTab: function (e) {
     var that = this;
     if (this.data.currentTab === e.target.dataset.current) {
@@ -23,59 +18,4 @@ Page({
       url: e.currentTarget.dataset.url,
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
