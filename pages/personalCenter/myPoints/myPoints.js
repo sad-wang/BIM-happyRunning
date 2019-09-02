c1884d110b80f0fd85f78ffd36c96d4ae8ba6206
Leown <leown_wang@163.com>
2019-09-02 11:15:11 +0800

    fix layout bug of echarts in myScore and myPoints pages

--- pages/personalCenter/myPoints/myPoints.js
+++ pages/personalCenter/myPoints/myPoints.js
@@ -1,72 +1,72 @@
 // pages/personalCenter/myPoints/myPoints.js
+
 import * as echarts from '../../../ec-canvas/echarts';
+const app = getApp();
 let chart = null;
 let optionData = null;
 //初始化 chart
 function initChart(canvas, width, height) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-  var option = optionData;
-  chart.setOption(option);
-  return chart;
+    chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+    });
+    canvas.setChart(chart);
+    var option = optionData;
+    chart.setOption(option);
+    return chart;
 }
 Page({
-  // 页面数据
-  data: {
-    currentTab: 0,
-    ec: null,
-    optionData:null
-  },
+    // 页面数据
+    data: {
+        currentTab: 0,
+        ec: null,
+        optionData:null
+    },
 
-  onLoad:function(options){
-    optionData = {
-      backgroundColor:'#fff',
-      color:['#c7ccd2', '#9457e6','#57d3e6'],
-      series: [
-        {
-          type:'pie',
-          radius: ['64%', '80%'],
-          label: {
-            normal: {
-              show: false,
-              position: 'center'
+    onLoad:function(options){
+        backgroundColor: "#ffffff",
+            optionData = {
+                color:['#9457e6','#57d3e6'],
+                series: [
+                    {
+                        type:'pie',
+                        radius: ['64%', '80%'],
+                        label: {
+                            normal: {
+                                show: false,
+                            },
+                        },
+                        data:[
+                            {value:20, name:'日常运动',},
+                            {value:70, name:'活动/比赛的参加'},
+                        ],
+                    }
+                ]
+            };
+        this.setData({
+            ec:{
+                onInit: initChart,
             },
-          },
-          data:[
-            {value:0, name:'',},
-            {value:20, name:'日常运动',},
-            {value:70, name:'活动/比赛的参加'},
-          ],
+            optionData:optionData,
+        })
+    },
+    //滑动切换
+    swiperTab: function (e) {
+        var that = this;
+        that.setData({
+            currentTab: e.detail.current
+        });
+    },
+    //点击切换
+    clickTab: function (e) {
+        console.log(1)
+        var that = this;
+        if (this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            that.setData({
+                currentTab: e.target.dataset.current
+            })
         }
-      ]
-    };
-    this.setData({
-      ec:{
-        onInit: initChart,
-      },
-      optionData:optionData,
-    })
-  },
-  //滑动切换
-  swiperTab: function (e) {
-    var that = this;
-    that.setData({
-      currentTab: e.detail.current
-    });
-  },
-  //点击切换
-  clickTab: function (e) {
-    console.log(1)
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
     }
-  }
 })
--- pages/personalCenter/myScore/myScore.js
+++ pages/personalCenter/myScore/myScore.js
@@ -1,27 +1,8 @@
 // pages/personalCenter/myScore/myScore.js
 import * as echarts from '../../../ec-canvas/echarts';
+const app = getApp();
 let chart = null;
-
-let optionData ={
-  color:['#c7ccd2', '#9457e6','#57d3e6'],
-  series: [
-    {
-      type:'pie',
-      radius: ['64%', '80%'],
-      label: {
-        normal: {
-          show: false,
-          position: 'center'
-        },
-      },
-      data:[
-        {value:0, name:'',},
-        {value:20, name:'日常运动',},
-        {value:70, name:'活动/比赛的参加'},
-      ],
-    }
-  ]
-};
+let optionData = null;
 //初始化 chart
 function initChart(canvas, width, height) {
   chart = echarts.init(canvas, null, {
@@ -29,38 +10,62 @@ function initChart(canvas, width, height) {
     height: height
   });
   canvas.setChart(chart);
-
   var option = optionData;
   chart.setOption(option);
   return chart;
 }
-
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   // 页面数据
   data: {
     currentTab: 0,
-    ec: {
-      onInit: initChart
-    },
-    optionData:optionData
+    ec: null,
+    optionData:null
   },
 
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-    },
-    //滑动切换
+  onLoad:function(options){
+    backgroundColor: "#ffffff",
+    optionData = {
+      color:['#9457e6','#57d3e6'],
+      series: [
+        {
+          type:'pie',
+          radius: ['64%', '80%'],
+          label: {
+            normal: {
+              show: false,
+            },
+          },
+          data:[
+            {value:20, name:'日常运动',},
+            {value:70, name:'活动/比赛的参加'},
+          ],
+        }
+      ]
+    };
+    this.setData({
+      ec:{
+        onInit: initChart,
+      },
+      optionData:optionData,
+    })
+  },
+  //滑动切换
   swiperTab: function (e) {
     var that = this;
     that.setData({
       currentTab: e.detail.current
     });
   },
+  //点击切换
+  clickTab: function (e) {
+    console.log(1)
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  }
 })
