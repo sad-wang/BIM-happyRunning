242492a959b850ce1e93e382b533d98a3035713d
Leown <leown_wang@163.com>
2019-09-02 10:23:21 +0800

    fiexed the echarts layout bug in myScore pages

--- pages/personalCenter/myScore/myScore.js
+++ pages/personalCenter/myScore/myScore.js
@@ -1,7 +1,27 @@
 // pages/personalCenter/myScore/myScore.js
 import * as echarts from '../../../ec-canvas/echarts';
 let chart = null;
-let optionData = null;
+
+let optionData ={
+  color:['#c7ccd2', '#9457e6','#57d3e6'],
+  series: [
+    {
+      type:'pie',
+      radius: ['64%', '80%'],
+      label: {
+        normal: {
+          show: false,
+          position: 'center'
+        },
+      },
+      data:[
+        {value:0, name:'',},
+        {value:20, name:'日常运动',},
+        {value:70, name:'活动/比赛的参加'},
+      ],
+    }
+  ]
+};
 //初始化 chart
 function initChart(canvas, width, height) {
   chart = echarts.init(canvas, null, {
@@ -9,63 +29,38 @@ function initChart(canvas, width, height) {
     height: height
   });
   canvas.setChart(chart);
+
   var option = optionData;
   chart.setOption(option);
   return chart;
 }
+
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   // 页面数据
   data: {
     currentTab: 0,
-    ec: null,
-    optionData:null
+    ec: {
+      onInit: initChart
+    },
+    optionData:optionData
   },
 
-  onLoad:function(options){
-    optionData = {
-      color:['#c7ccd2', '#9457e6','#57d3e6'],
-      series: [
-        {
-          type:'pie',
-          radius: ['64%', '80%'],
-          label: {
-            normal: {
-              show: false,
-              position: 'center'
-            },
-          },
-          data:[
-            {value:0, name:'',},
-            {value:20, name:'日常运动',},
-            {value:70, name:'活动/比赛的参加'},
-          ],
-        }
-      ]
-    };
-    this.setData({
-      ec:{
-        onInit: initChart,
-      },
-      optionData:optionData,
-    })
-  },
-  //滑动切换
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    },
+    //滑动切换
   swiperTab: function (e) {
     var that = this;
     that.setData({
       currentTab: e.detail.current
     });
   },
-  //点击切换
-  clickTab: function (e) {
-    console.log(1)
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  }
 })
