49194f1e899daa8289d6341cf0099fc10b251e7d
GraceWalk <gracewalk1999@163.com>
2019-09-10 11:30:48 +0800

    rewrite schedule of dailyAttendance page

--- pages/dailyAttendance/dailyAttendance.js
+++ pages/dailyAttendance/dailyAttendance.js
@@ -1,69 +1,64 @@
-let rpx;
+let rpx, speed, pct, scheduleFor,
+    ctx, lineargradient;
+speed = 1;
+pct = 80;
+ctx = wx.createCanvasContext('scheduleCanvas');
+lineargradient = ctx.createLinearGradient(10, 268, 590, 268);
+lineargradient.addColorStop(0, 'rgba(61, 183, 199, 1)');
+lineargradient.addColorStop(1, 'rgba(1, 189, 142, 1)');
+
 //获取屏幕宽度，获取自适应单位
 wx.getSystemInfo({
   success: function(res) {
     rpx = res.windowWidth / 750;
   },
 })
-// let speed = 1,
-//     pct = 80;
-// let lineargradient = ctx.createLinearGradient(10, 268, 590, 268);
-// lineargradient.addColorStop(0, 'rgba(61, 183, 199, 1)');
-// lineargradient.addColorStop(1, 'rgba(1, 189, 142, 1)');
 
-
-// //绘制当前进度条
-// function writeSchedule(n) {
-//   ctx.save();
-//   ctx.setStrokeStyle(lineargradient);
-//   ctx.beginPath();
-//   ctx.arc(300, 268, 258, Math.PI, (-Math.PI / 100) * (100 - n), false);
-//   ctx.fill("evenodd");
-//   ctx.stroke();
-//   ctx.restore();
-// }
-
-//绘制进度条背景
-function writeBackground(ctx) {
+Page({
+  data: {
+    hidden: 0,
+    mileData: 8.07
+},
+writeBackground: function() {
   ctx.save();
   ctx.setStrokeStyle("#e7eaeb");
   ctx.beginPath();
   ctx.arc(300 * rpx, 268 * rpx, 258 * rpx, Math.PI, 0, false);
   ctx.fill("evenodd");
   ctx.stroke();
-  ctx.draw();
   ctx.restore();
-}
-
-// (function drawFrame() {
-//   ctx.requestAnimationFrame(drawFrame);
-//   writeSchedule(speed);
-
-//   if (speed > pct) speed = 0;
-//   speed += 1;
-// }());
-
-Page({
-  data: {
-    hidden: 0
+  ctx.draw();
+},
+writeSchedule: function() {
+  var that = this;
+  ctx.save();
+  ctx.setStrokeStyle(lineargradient);
+  ctx.beginPath();
+  ctx.arc(300 * rpx, 268 * rpx, 258 * rpx, Math.PI, (-Math.PI / 100) * (100 - speed++), false);
+  ctx.fill("evenodd");
+  ctx.stroke();
+  ctx.restore();
+  ctx.draw(true);
+  if(speed >= (that.data.mileData * 10)) {
+    clearInterval(scheduleFor);
+  }
 },
 onReady: function(e) {
-  let ctx = wx.createCanvasContext('scheduleCanvas');
-
   ctx.setLineWidth(20 * rpx);
   ctx.setFillStyle("rgba(0, 0, 0, 0)");
   ctx.setLineCap('round');
 
-  writeBackground(ctx);
-  // ctx.requestAnimationFrame(drawFrame);
+  this.writeBackground();
+  scheduleFor = setInterval(this.writeSchedule, 16);
 },
+
 onLoad: function (options) {
   var that = this;
   setTimeout(function() {
     that.setData({
       hidden: 1
     })
-  }, 3000)
+  }, 3000);
 },
 onShow:function () {
 }
