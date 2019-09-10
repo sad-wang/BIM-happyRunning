81a1759463a48cee4ac0e9fb59bf0a52910eca85
Leown <leown_wang@163.com>
2019-09-10 15:21:25 +0800

    optimized search box in mooc pages

--- pages/mooc/mooc.js
+++ pages/mooc/mooc.js
@@ -1,10 +1,22 @@
 // pages/mooc/mooc.js
 Page({
   data: {
+    searchBoxState:false,
   },
   navTo:function(e) {
     wx.navigateTo({
       url: e.currentTarget.dataset.url,
     })
   },
+  changeSearchBoxState:function (e) {
+    this.setData({
+      searchBoxState:true
+    })
+  },
+  confirmSearch:function (e) {
+    this.setData({
+      searchBoxState:false
+    })
+
+  }
 })
