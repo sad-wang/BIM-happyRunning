51e985c074f65428b140f2e0cbcf1c2d0ca29f0b
Leown <leown_wang@163.com>
2019-09-25 11:58:48 +0800

    update

--- PC-management/pc-management-static/src/js/main.js
+++ PC-management/pc-management-static/src/js/main.js
@@ -1,8 +1,30 @@
 let form
-layui.use('form', function(){
+let table
+layui.use(['form','table'], function(){
     form = layui.form;
-
-    //各种基于事件的操作，下面会有进一步介绍
+    table = layui.table;
+    //监听表格复选框选择
+    table.on('checkbox(demo)', function(obj){
+        console.log(obj)
+    });
+    //监听工具条
+    table.on('tool(demo)', function(obj){
+        var data = obj.data;
+        if(obj.event === 'detail'){
+            layer.msg('ID：'+ data.id + ' 的查看操作');
+        } else if(obj.event === 'del'){
+            layer.confirm('真的删除行么', function(index){
+                obj.del();
+                layer.close(index);
+            });
+        } else if(obj.event === 'edit'){
+            layer.alert('编辑行：<br>'+ JSON.stringify(data))
+        }
+    });
+    $('.demoTable .layui-btn').on('click', function(){
+        var type = $(this).data('type');
+        active[type] ? active[type].call(this) : '';
+    });
 });
 
 new Vue({
